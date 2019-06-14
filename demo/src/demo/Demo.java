c26ea8abf96fafb4fc6f0393c8014a3e76c3e173
zhangyan <zhangyan@DESKTOP-LMNG36T>
2019-06-14 11:10:26 +0800

    优化代码，删除了部分无用注释

--- demo/src/demo/Demo.java
+++ demo/src/demo/Demo.java
@@ -3,7 +3,6 @@ package demo;
 public class Demo {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		System.out.println("Hello World");
 		System.out.println("demo2");
 	}
